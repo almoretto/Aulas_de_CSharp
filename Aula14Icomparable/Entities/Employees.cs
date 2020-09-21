0456afa935e361b2ef923cb0229ea4d5d319e867
amoretto <amoretto@outlook.com.br>
2020-09-21 19:37:12 -0300

    Aula 14 Icomparable

---
+++ Aula14Icomparable/Entities/Employees.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using System;
+
+namespace Aula14Icomparable.Entities
+{
+    class Employees : IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employees(string csvEmployee)
+        {
+            string[] emp = csvEmployee.Split(';');
+            Name = emp[0];
+            Salary = double.Parse(emp[1], CultureInfo.InvariantCulture);
+
+        }
+
+        public override string ToString()
+        {
+            return Name + ", "
+                + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Employees))
+            {
+                throw new ArgumentException("Comparring error: Argument is not an Employee");
+            }
+            Employees other = obj as Employees;
+            return Name.CompareTo(other.Name);
+        }
+    }
+}
---
+++ Aula14Icomparable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Aula14Icomparable.Entities;
+
+namespace Aula14Icomparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\func.csv";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employees> list = new List<Employees>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employees(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Employees str in list)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
