875a240a952fd024570bec08ba8c50888b174c3e
amoretto <amoretto@outlook.com.br>
2020-09-27 09:15:37 -0300

    Aula 15 Hashcode Equals

---
+++ Aula15HashCodeEquals/Entities/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula15HashCodeEquals.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Client))
+            {
+                return false;
+            }
+            Client test = obj as Client; // DownCasting
+            return Email.Equals(test.Email);
+        }
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ Aula15HashCodeEquals/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Aula15HashCodeEquals.Entities;
+
+namespace Aula15HashCodeEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client
+            {
+                Name = "André",
+                Email = "amoretto@outlook.com.br"
+            };
+            Client b = new Client
+            {
+                Name = "Mary",
+                Email = "mmoretto@outlook.com.br"
+            };
+            Client c = new Client
+            {
+                Name = "André",
+                Email = "amoretto@outlook.com.br"
+            };
+            Console.Write("a.Equals(b): ");
+            Console.WriteLine(a.Equals(b));
+            Console.Write("a==b: ");
+            Console.WriteLine(a == b);
+            Console.Write("a.Equals(c): ");
+            Console.WriteLine(a.Equals(c));
+            Console.Write("a==c: ");
+            Console.WriteLine(a == c);
+            int hCa = a.GetHashCode();
+            int hCb = b.GetHashCode();
+            int hCc = c.GetHashCode();
+
+            Console.WriteLine("Hashcode axb: " + (hCa == hCb).ToString());
+            Console.WriteLine("Hashcode axc:" + (hCa == hCc).ToString());
+        }
+    }
+}
