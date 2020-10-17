bde15e3fdcdcc5fe4b660acba87ba19de689f8a8
amoretto <amoretto@outlook.com.br>
2020-10-17 09:05:19 -0300

    Aula 17 Func with select

--- Aula17Predicate/Program.cs
+++ Aula17Predicate/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Aula17Predicate.Entities;
+using System.Linq;
 
 namespace Aula17Predicate
 {
@@ -13,8 +14,17 @@ namespace Aula17Predicate
             list.Add(new Product("Mouse", 50.00));
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
+
+            /*Func<Product, string> func =  *NameUpper* p => p.Name.ToUpper();*/
+            // Declaration of the Delegate Func using the Method NameUpper in comments *NameUpper*
+            // The declaration is completely unneeded if we put the Lambda in the select method
+            //The better way is to use the Lambda Expression p=>p.Name.ToUpper();
+            List<string> newList = list.Select(p => p.Name.ToUpper()/*NameUpper*/ /*func*/).ToList();//Delegate Func with Select 
+            //need to convert the result ToList() because the result is TResult IEnumerable.
+
             Action<Product> act = UpdatePrice; //Create Action
             list.ForEach(act);
+
             list.ForEach(UpdatePrice); //Action Delegate
             foreach (Product item in list)
             {
@@ -27,7 +37,9 @@ namespace Aula17Predicate
                 Console.WriteLine(item);
             }
             //Solution with Delegate Function
+
             list.RemoveAll(ProductTest);
+
 
         }
         public static bool ProductTest(Product p)
@@ -37,6 +49,10 @@ namespace Aula17Predicate
         public static void UpdatePrice(Product p)
         {
             p.Price += p.Price * 0.1;
+        }
+        public static string NameUpper(Product p)
+        {
+            return p.Name.ToUpper();
         }
     }
 }
